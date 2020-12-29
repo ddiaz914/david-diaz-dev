eb7cbc22b5167fd92416cf98ce75abafff18cd00
David Diaz <ddiaz914@gmail.com>
2020-12-29 15:23:11 -0600

    Replace videos with sprite animations

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,42 +1,15 @@
 <template>
   <div class="flex items-center justify-center mt-5 md:mt-0 md:h-screen">
-    <div
-      class="david"
-      :class="{'fadeIn': videoReady}"
-      v-show="videoReady"
-    >
-      <video
-        ref="waving"
-        autoplay
-        playsinline
-        v-show="!wavingComplete"
-        class="z-10"
-        width="192"
-        height="498"
-        muted="muted"
-      >
-        <source
-          src="../assets/videos/waving.mp4"
-          type="video/mp4"
-        >
-        Your browser does not support the video tag.
-      </video>
-      <video
-        ref="blinking"
-        autoplay
-        playsinline
-        v-show="wavingComplete"
-        class="z-10"
-        width="192"
-        height="498"
-        muted="muted"
-      >
-        <source
-          src="../assets/videos/blink.mp4"
-          type="video/mp4"
-        >
-        Your browser does not support the video tag.
-      </video> 
+    <div class="relative">
+      <div class="stationary"></div>
+      <div
+        v-if="waveAnimationComplete"
+        class="blink">
+      </div>
+      <div
+        v-if="!waveAnimationComplete"
+        class="wave">
+      </div>
     </div>
     <div class="flex flex-col">
       <h1 class="font-display text-4xl sm:text-6xl leading-none">
@@ -77,58 +50,13 @@ export default {
   name: 'Home',
   data() {
     return {
-      videoReady: false,
-      wavingComplete: false
+      waveAnimationComplete: false
     }
   },
   mounted() {
-    const wavingVideo = this.$refs.waving
-    wavingVideo.addEventListener('loadeddata', () => {
-      if (wavingVideo.readyState === 4) {
-        this.videoReady = true
-        wavingVideo.pause()
-      } 
-    })
-  },
-  watch: {
-    videoReady() {
-      window.setTimeout(() => {
-        this.playWaving()
-      }, 2000)
-    }
-  },
-  methods: {
-    playWaving() {
-      const wavingVideo = this.$refs.waving
-      wavingVideo.play()
-      window.setTimeout(() => {
-        wavingVideo.pause()
-      }, 500)
-      window.setTimeout(() => {
-        wavingVideo.play()
-      }, 4000)
-      wavingVideo.addEventListener('ended', () => {
-        this.wavingComplete = true
-        this.startBlinkInterval()
-      })
-    },
-    startBlinkInterval() {
-      const blinkingVideo = this.$refs.blinking
-      blinkingVideo.play()
-      window.setInterval(() => {
-        blinkingVideo.play()
-      }, 5000)
-    }
+    window.setTimeout(() => {
+      this.waveAnimationComplete = true
+    }, 5000)
   }
 }
 </script>
-<style>
-.david {
-  background-image: url('../assets/images/david-diaz.png');
-  background-repeat: no-repeat;
-  background-position: top left;
-  background-size: 192px 498px;
-  width: 192px;
-  height: 498px;
-}
-</style>
